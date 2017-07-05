b970706875dbd87bbf0fd0e7ef48b8f6459efd87
shiliangL <1462999406@qq.com>
2017-07-05 23:25:51 +0800

    'add-kc'

--- src/page/KC-Manage/KC-Item01/product-bind.vue
+++ src/page/KC-Manage/KC-Item01/product-bind.vue
@@ -1,25 +1,31 @@
 <template>
     <div id="product-bind">
-        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" :inline="true" label-position="left">
-            <el-form-item label="产品名称" prop="productName">
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="ruleForm" :inline="true" label-position="left">
+            <el-form-item label="入库批次" prop="productName">
                 <el-input v-model="ruleForm.productName"></el-input>
             </el-form-item>
-            <el-form-item label="通讯方式" prop="communicationType">
-                <el-input v-model="ruleForm.communicationType"></el-input>
-            </el-form-item>
-            <!--<el-form-item label="Product Key" prop="ProductKey">
-                <el-input v-model="ruleForm.ProductKey"></el-input>
-            </el-form-item>
-            <el-form-item label="Product Secret" prop="ProductSecret">
-                <el-input v-model="ruleForm.ProductSecret"></el-input>
-            </el-form-item> -->
-        </el-form>
-        <div style=" text-align: center">
-            <el-button @click="closeForm">取消</el-button>
-            <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-        </div>
-        <loading v-if="loading" @loadingRefresh="onRefresh" :loadingText="loadingText"></loading>
-    </div>
+            <el-form-item label="类型">
+                <el-select v-model="ruleForm.productName" placeholder="请选择">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+          </el-form-item>
+          <el-form-item label="型号">
+            <el-select v-model="ruleForm.productName" placeholder="请选择">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+      </el-form-item>
+      <el-form-item label="数量" prop="communicationType">
+        <el-input v-model="ruleForm.communicationType"></el-input>
+    </el-form-item>
+</el-form>
+<div style=" text-align: center">
+    <el-button @click="closeForm">取消</el-button>
+    <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+</div>
+<loading v-if="loading" @loadingRefresh="onRefresh" :loadingText="loadingText"></loading>
+</div>
 </template>
 <script>
     export default {
@@ -69,19 +75,19 @@
         submitForm(formName) { //表单提交
             this.$refs[formName].validate((valid) => { //验证表单数据是否合法
                 if (valid) {
-                   this.ruleForm.time= new Date().toLocaleString();
-                   this.$emit('addData',this.ruleForm);
-                   this.$message({
+                 this.ruleForm.time= new Date().toLocaleString();
+                 this.$emit('addData',this.ruleForm);
+                 this.$message({
                     message: '添加成功',
                     type: 'success'
                 });
-                   this.total += 1;
-                   this.$emit('close');
-                   close
-               } else {
+                 this.total += 1;
+                 this.$emit('close');
+                 close
+             } else {
 
-               }
-           })
+             }
+         })
         },
         getEditData() {
 
@@ -93,13 +99,11 @@
 }
 </script>
 <style scoped lang="scss">
-    #product-bind {
-        .el-form{
-            text-align: center;
-        }
-        .el-form-item {
-            display:inline-block;
-            width: 280px;
-        }
+    .ruleForm {
+        text-align: center;
+        .el-input,
+        .el-select{
+            width:180px;
+        };
     }
 </style>
