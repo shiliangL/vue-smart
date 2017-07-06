b998a91dcf613c23844d721ed3f6a1651d5d831a
shiliangL <1462999406@qq.com>
2017-07-06 23:33:42 +0800

    'add-pages'

--- src/page/KC-Manage/KC-Item01/KC-Item01.vue
+++ src/page/KC-Manage/KC-Item01/KC-Item01.vue
@@ -1,134 +1,144 @@
 <template>
-    <div id="KC-Item01">
-        <!-- <h1>厂家库存</h1> -->
-        <div class="search">
-            <ul>
-                <li>
-                    <el-input placeholder="产品名称检索" v-model.tirm="searchName" />
-                </li>
-                <li>
-                    <el-button type="primary" class="mb-sm btn btn-primary" @click="search">查询</el-button>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <el-button type="primary" class="mb-sm btn btn-primary btn-outline" @click="handleEdit(0,1)">新增入库</el-button>
-                </li>
-            </ul>
-        </div>
-        <el-table ref="singleTable" :data="tableData" highlight-current-row @current-change="handleCurrentChange" style="width: 100%">
-            <el-table-column type="index" width="50" align="center">
-            </el-table-column>
-            <el-table-column property="productName" label="产品名称" align="center">
-            </el-table-column>
-            <el-table-column property="communicationType" label="通讯方式" align="center">>
-            </el-table-column>
-            <el-table-column property="time" label="绑定时间" align="center">>
-            </el-table-column>
-            <el-table-column label="操作" align="center">
-                <template scope="scope" align="center">
-                    <el-button size="mini" class="btn-edit" @click="handleEdit(scope.$index, 2)">查看详情</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-        <div class="pages block" style="margin-top: 20px">
-            <el-pagination @current-change="handleCurrentChange" layout="total, prev, pager, next" :current-page="currentPage" :page-size="10" :total="3" align="center">
-            </el-pagination>
-        </div>
-        <el-dialog v-model="dialogFormVisible" :size="size" :title="dialogTitle" @close="dismiss">
-            <add-form v-if="showForm" @close="dismiss" @refreshPage="onRefresh" @addData="addData"></add-form>
-            <add-form-details v-if="showDetails" @close="dismiss" @refreshPage="onRefresh"></add-form-details>
-        </el-dialog>
+  <div id="KC-Item01">
+    <!-- <h1>厂家库存</h1> -->
+    <div class="search">
+      <ul>
+        <li>
+          <el-input placeholder="产品名称检索" v-model.tirm="searchName" />
+        </li>
+        <li>
+          <el-button type="primary" class="mb-sm btn btn-primary" @click="search">查询</el-button>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <el-button type="primary" class="mb-sm btn btn-primary btn-outline" @click="handleEdit(0,1)">新增入库</el-button>
+        </li>
+      </ul>
     </div>
+    <el-table ref="singleTable" :data="tableData" highlight-current-row @current-change="handleCurrentChange" style="width: 100%">
+      <el-table-column type="index" width="50" align="center">
+      </el-table-column>
+      <el-table-column property="productName" label="产品名称" align="center">
+      </el-table-column>
+      <el-table-column property="communicationType" label="通讯方式" align="center">>
+      </el-table-column>
+      <el-table-column property="time" label="绑定时间" align="center">>
+      </el-table-column>
+      <el-table-column label="操作" align="center">
+        <template scope="scope" align="center">
+          <el-button size="mini" class="btn-edit" @click="handleEdit(scope.$index, 2)">查看详情</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="pages block" style="margin-top: 20px">
+      <el-pagination @current-change="handleCurrentChange" layout="total, prev, pager, next" :current-page="currentPage" :page-size="10" :total="3" align="center">
+      </el-pagination>
+    </div>
+    <el-dialog v-model="dialogFormVisible" :size="size" :title="dialogTitle" @close="dismiss">
+      <add-form v-if="showForm" @close="dismiss" @refreshPage="onRefresh" @addData="addData"></add-form>
+      <add-form-details v-if="showDetails" @close="dismiss" @refreshPage="onRefresh"></add-form-details>
+    </el-dialog>
+  </div>
 </template>
 <script>
-    import { init, destroy } from 'ityped';
-    import AddForm from './product-bind.vue'
-    import AddFormDetails from './showDetails.vue'
-    export default {
-        components: {
-            AddForm,
-            AddFormDetails
-        },
-        data() {
-            return {
-                currentPage: 1,
-                tableData: [{
-                    productName: '产品 A',
-                    communicationType: 'COAP',
-                    time: '2017-7-2 16:26:08'
-                },{
-                    productName: '产品 A',
-                    communicationType: 'COAP',
-                    time: '2017-7-2 16:26:08'
-                },{
-                    productName: '产品 A',
-                    communicationType: 'COAP',
-                    time: '2017-7-2 16:26:08'
-                }],
-                currentRow: null,
-                searchName: '',
+  import { init, destroy } from 'ityped';
+  import AddForm from './product-bind.vue'
+  import AddFormDetails from './showDetails.vue'
 
-                size: '',
-                dialogTitle: '',
-                showForm: false,
-                showDetails: false,
-                dialogFormVisible: false,
+  export default {
+    components: {
+      AddForm,
+      AddFormDetails,
+    },
+    data() {
+      return {
+        currentPage: 1,
+        tableData: [{
+          productName: '产品 A',
+          communicationType: 'COAP',
+          time: '2017-7-2 16:26:08'
+        },{
+          productName: '产品 A',
+          communicationType: 'COAP',
+          time: '2017-7-2 16:26:08'
+        },{
+          productName: '产品 A',
+          communicationType: 'COAP',
+          time: '2017-7-2 16:26:08'
+        }],
+        currentRow: null,
+        searchName: '',
 
-            }
-        },
-        methods: {
-            dismiss() {
-                this.showForm = false;
-                this.showDetails = false;
-                this.dialogFormVisible = false;
-            },
-            onRefresh() {
+        size: '',
+        dialogTitle: '',
+        showForm: false,
+        showDetails: false,
+        dialogFormVisible: false,
 
-            },
-            setCurrent(row) {
-                this.$refs.singleTable.setCurrentRow(row);
-            },
-            handleCurrentChange(val) {
-                this.currentRow = val;
-            },
-            handleEdit(index, type) {
-                switch (type) {
-                    case 1:
+      }
+    },
+    methods: {
+      dismiss() {
+        this.showForm = false;
+        this.showDetails = false;
+        this.dialogFormVisible = false;
+      },
+      onRefresh() {
+
+      },
+      setCurrent(row) {
+        this.$refs.singleTable.setCurrentRow(row);
+      },
+      handleCurrentChange(val) {
+        this.currentRow = val;
+      },
+      handleEdit(index, type) {
+        switch (type) {
+          case 1:
                     { //编辑
-                        console.log('新绑定');
-                        this.size = 'tiny';
-                        this.showForm = true;
-                        this.showDetails = false;
-                        this.dialogFormVisible = true;
-                        break;
+                      console.log('新绑定');
+                      this.size = 'tiny';
+                      this.showForm = true;
+                      this.showDetails = false;
+                      this.dialogFormVisible = true;
+                      break;
                     }
                     case 2:
                     {
-                        this.size = 'full'
-                        this.dialogTitle = '产品详细参数详情';
-                        this.showForm = false;
-                        this.showDetails = true;
-                        this.dialogFormVisible = true;
-                        break;
+                      this.size = 'full'
+                      this.dialogTitle = '产品详细参数详情';
+                      this.showForm = false;
+                      this.showDetails = true;
+                      this.dialogFormVisible = true;
+                      break;
                     }
-                }
-            },
-            search() {
+                  }
+                },
+                search() {
 
-            },
-            addData(item){
-                this.tableData.push(item)
+                },
+                addData(item){
+                  this.tableData.push(item)
+                }
+              },
+              directives:{
+                focus:{
+                  inserted: function (el) {
+              // 聚焦元素
+              el.focus()
             }
-        },
-    }
-</script>
-<style scoped lang="scss">
-    #de-list {
+          }
+        }
+
+      }
+    </script>
+    <style scoped lang="scss">
+      #de-list {
         padding: 10px;
         .shi{
-            height:50px;
-            border: 1px solid red;
+          height:50px;
+          border: 1px solid red;
         }
-    }
-</style>
+      }
+    </style>
--- src/page/KC-Manage/KC-Item02/product-bind.vue
+++ src/page/KC-Manage/KC-Item02/product-bind.vue
@@ -1,31 +1,25 @@
 <template>
     <div id="product-bind">
-    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="ruleForm" :inline="true">
-            <el-form-item label="入库批次" prop="productName">
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" :inline="true" label-position="left">
+            <el-form-item label="产品名称" prop="productName">
                 <el-input v-model="ruleForm.productName"></el-input>
             </el-form-item>
-            <el-form-item label="类型">
-                <el-select v-model="ruleForm.productName" placeholder="请选择">
-                  <el-option label="区域一" value="shanghai"></el-option>
-                  <el-option label="区域二" value="beijing"></el-option>
-              </el-select>
-          </el-form-item>
-          <el-form-item label="型号">
-            <el-select v-model="ruleForm.productName" placeholder="请选择">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
-      </el-form-item>
-      <el-form-item label="数量" prop="communicationType">
-        <el-input v-model="ruleForm.communicationType"></el-input>
-    </el-form-item>
-</el-form>
-<div style=" text-align: center">
-    <el-button @click="closeForm">取消</el-button>
-    <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-</div>
-<loading v-if="loading" @loadingRefresh="onRefresh" :loadingText="loadingText"></loading>
-</div>
+            <el-form-item label="通讯方式" prop="communicationType">
+                <el-input v-model="ruleForm.communicationType"></el-input>
+            </el-form-item>
+            <!--<el-form-item label="Product Key" prop="ProductKey">
+                <el-input v-model="ruleForm.ProductKey"></el-input>
+            </el-form-item>
+            <el-form-item label="Product Secret" prop="ProductSecret">
+                <el-input v-model="ruleForm.ProductSecret"></el-input>
+            </el-form-item> -->
+        </el-form>
+        <div style=" text-align: center">
+            <el-button @click="closeForm">取消</el-button>
+            <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        </div>
+        <loading v-if="loading" @loadingRefresh="onRefresh" :loadingText="loadingText"></loading>
+    </div>
 </template>
 <script>
     export default {
@@ -75,19 +69,19 @@
         submitForm(formName) { //表单提交
             this.$refs[formName].validate((valid) => { //验证表单数据是否合法
                 if (valid) {
-                 this.ruleForm.time= new Date().toLocaleString();
-                 this.$emit('addData',this.ruleForm);
-                 this.$message({
+                   this.ruleForm.time= new Date().toLocaleString();
+                   this.$emit('addData',this.ruleForm);
+                   this.$message({
                     message: '添加成功',
                     type: 'success'
                 });
-                 this.total += 1;
-                 this.$emit('close');
-                 close
-             } else {
+                   this.total += 1;
+                   this.$emit('close');
+                   close
+               } else {
 
-             }
-         })
+               }
+           })
         },
         getEditData() {
 
@@ -99,14 +93,13 @@
 }
 </script>
 <style scoped lang="scss">
-    .ruleForm {
-        text-align: center;
-        .el-form-item{
-           display: block;
-           .el-input,
-           .el-select{
-            width:180px;
-        };
+    #product-bind {
+        .el-form{
+            text-align: center;
+        }
+        .el-form-item {
+            display:inline-block;
+            width: 280px;
+        }
     }
-}
 </style>
